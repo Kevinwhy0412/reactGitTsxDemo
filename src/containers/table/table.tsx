327798f91a291fa938409bc958a7224b28d3539a
Kevinwhy0412 <1094502321@qq.com>
2019-07-19 17:24:41 +0800

    antd表格

--- src/containers/table/table.tsx
+++ src/containers/table/table.tsx
@@ -1,35 +1,49 @@
 import React, { Component } from 'react';
 import { Table, Divider, Tag } from 'antd';
 
-class TableList extends React.Component {
+import tableStyle from './table.module.scss';
+
+const rowSelection = {
+    onChange: (selectedRowKeys: any, selectedRows: any) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    }
+};
+
+interface tabprops {
+    none: string
+}
+interface tabstate {
+    data: any
+}
+class TableList extends React.Component<tabprops, tabstate>{
+    constructor(tabprops: any) {
+        super(tabprops);
+        this.state = {
+            data: this.datar
+        }
+    }
     public columns = [
         {
             title: 'Name',
             dataIndex: 'name',
-            key: 'name',
-            render: (text: React.ReactNode) => <a href="javascript:;">{text}</a>,
+            render: (text: any) => <span>{text}</span>,
         },
         {
             title: 'Age',
             dataIndex: 'age',
-            key: 'age',
         },
         {
             title: 'Address',
             dataIndex: 'address',
-            key: 'address',
         },
         {
             title: 'Tags',
-            key: 'tags',
             dataIndex: 'tags',
             render: (tags: any) => (
                 <span>
                     {tags.map((tag: any) => {
-                        let color = tag.length > 5 ? 'geekblue' : 'green';
-                        if (tag === 'loser') {
-                            color = 'volcano';
-                        }
+                        let color = tag.length > 5 ? 'red' : 'green';
+                        //tag是字符串?!。。。。。
                         return (
                             <Tag color={color} key={tag}>
                                 {tag.toUpperCase()}
@@ -41,43 +55,57 @@ class TableList extends React.Component {
         },
         {
             title: 'Action',
-            key: 'action',
+            dataIndex: 'action',
             render: (text: any, record: any) => (
                 <span>
-                    <a href="javascript:;">Invite {record.name}</a>
-                    <Divider type="vertical" />
-                    <a href="javascript:;">Delete</a>
+                    <a href="javascript:;" onClick={this.deleteRow.bind(this, record.key)}>{record.oper}</a>
                 </span>
             ),
         },
     ]
-    public data = [
+    public datar = [
         {
-            key: '1',
+            key: 'a',
             name: 'John Brown',
             age: 32,
             address: 'New York No. 1 Lake Park',
             tags: ['nice', 'developer'],
+            oper: "删除1"
         },
         {
-            key: '2',
+            key: 'b',
             name: 'Jim Green',
             age: 42,
             address: 'London No. 1 Lake Park',
             tags: ['loser'],
+            oper: "删除2"
         },
         {
-            key: '3',
+            key: 'c',
             name: 'Joe Black',
             age: 32,
             address: 'Sidney No. 1 Lake Park',
             tags: ['cool', 'teacher'],
+            oper: "删除3"
         },
     ];
-    render() {
+
+    deleteRow(index: any, event: any) {
+        let keys: any[] = [];
+        this.state.data.forEach((i: any, num: any) => {
+            return keys[num] = i.key;
+        })
+        let n = keys.indexOf(index);
+        this.datar.splice(n, 1);
+        this.setState({ data: this.datar })
+        keys.splice(n, 1)
+    }
+    public render() {
+        const { data } = this.state;
         return (
-            <div className="tab">
-                <Table columns={this.columns} dataSource={this.data} />
+            <div className={tableStyle.taber}>
+                {/* {JSON.stringify(data)} */}
+                <Table rowSelection={rowSelection} columns={this.columns} dataSource={data} bordered size="middle" />
             </div>
         );
     }
